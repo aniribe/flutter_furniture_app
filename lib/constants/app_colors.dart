b2729175388d9d86c0e9e4db9b10d7c0fb66e164
aniribe <irinabelousova11@gmail.com>
2022-09-04 18:24:51 +0400

    Labraries were added; Files with constants were added; Services for fetch API were added;

---
+++ lib/constants/app_colors.dart
@@ -0,0 +1,8 @@
+import 'dart:ui';
+
+class AppColors {
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color primaryColor = Color(0xFFA95EFA);
+  static const Color secondaryColor = Color(0xFFF3F6F8);
+  static const Color textColor = Color(0xFF171717);
+}
---
+++ lib/constants/app_images.dart
@@ -0,0 +1,10 @@
+class AppImages {
+  static const String arrowIcon = 'assets/images/arrow.svg';
+  static const String bagIcon = 'assets/images/bag.svg';
+  static const String checkIcon = 'assets/images/check.svg';
+  static const String homeIcon = 'assets/images/home.svg';
+  static const String menuIcon = 'assets/images/menu.svg';
+  static const String scanIcon = 'assets/images/scan.svg';
+  static const String searchIcon = 'assets/images/search.svg';
+  static const String userIcon = 'assets/images/user.svg';
+}
